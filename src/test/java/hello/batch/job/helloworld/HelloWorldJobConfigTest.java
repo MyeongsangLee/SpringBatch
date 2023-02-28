65276a634260edc6c39f5f1f6e2f3305ab026efb
Lee Myeongsang <mslee0052@gmail.com>
2023-02-28 17:41:34 +0900

    commit

---
+++ src/test/java/hello/batch/job/helloworld/HelloWorldJobConfigTest.java
@@ -0,0 +1,10 @@
+package hello.batch.job.helloworld;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringR)
+class HelloWorldJobConfigTest {
+
+}
