5e49c75e4e31e944b78275caaf759ef0b145a2ca
Lee Myeongsang <mslee0052@gmail.com>
2023-03-07 17:58:51 +0900

    commit

--- src/test/java/hello/batch/job/dbdatareadwrite/TrMigrationConfigTest.java
+++ src/test/java/hello/batch/job/dbdatareadwrite/TrMigrationConfigTest.java
@@ -1,5 +1,8 @@
-package hello.batch.job.helloworld;
+package hello.batch.job.dbdatareadwrite;
 
+import hello.batch.core.domain.accounts.AccountsRepository;
+import hello.batch.core.domain.orders.OrdersRepository;
+import hello.batch.job.helloworld.SpringBatchTestConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -9,22 +12,34 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBatchTest
-@SpringBootTest(classes = {SpringBatchTestConfig.class, HelloWorldJobConfig.class})
-class HelloWorldJobConfigTest {
+@SpringBootTest(classes = {SpringBatchTestConfig.class, TrMigrationConfig.class})
+class TrMigrationConfigTest {
+
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
-    @Test
-    public void success() throws Exception {
+    @Autowired
+    private OrdersRepository ordersRepository;
+
+    @Autowired
+    private AccountsRepository accountsRepository;
+
+    @Test()
+    public void success_noData() throws Exception {
+        //when
         JobExecution execution = jobLauncherTestUtils.launchJob();
 
+        //then
         Assertions.assertEquals(execution.getExitStatus(), ExitStatus.COMPLETED);
-    }
+        Assertions.assertEquals(0, accountsRepository.count());
 
+    }
 }
